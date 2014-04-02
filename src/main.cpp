fb8f03b31e5d702eb0b55031a8024222c01b23e4
Olivia Neacsu <olivia.neacsu@gmail.com>
2014-04-02 17:22:08 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 31, 2014
+ *      Author: oli
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
+
